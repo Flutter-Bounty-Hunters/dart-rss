4e890bc38a20c3523acd43a82b7bf0ed53ffb685
N <nirvan@gmail.com>
2021-04-05 16:45:33 -0700

    Update group.dart

--- lib/domain/media/group.dart
+++ lib/domain/media/group.dart
@@ -2,37 +2,61 @@ import 'package:dart_rss/domain/media/category.dart';
 import 'package:dart_rss/domain/media/content.dart';
 import 'package:dart_rss/domain/media/credit.dart';
 import 'package:dart_rss/domain/media/rating.dart';
-import 'package:dart_rss/util/helpers.dart';
+import 'package:dart_rss/domain/media/thumbnail.dart';
+import 'package:dart_rss/domain/media/title.dart';
+import 'package:dart_rss/domain/media/description.dart';
+import 'package:dart_rss/domain/media/community.dart';
+import 'package:dart_rss/util/xml.dart';
 import 'package:xml/xml.dart';
 
 class Group {
-  final List<Content> contents;
-  final List<Credit> credits;
+  final Title? title;
+  final List<Content>? contents;
+  final List<Thumbnail>? thumbnails;
+  final Description? description;
+  final Community? community;
+  final List<Credit>? credits;
   final Category? category;
   final Rating? rating;
 
-  const Group({
-    this.contents = const <Content>[],
-    this.credits = const <Credit>[],
+  Group({
+    this.title,
+    this.contents,
+    this.thumbnails,
+    this.description,
+    this.community,
+    this.credits,
     this.category,
     this.rating,
   });
 
-  static Group? parse(XmlElement? element) {
-    if (element == null) {
-      return null;
-    }
+  static parse(XmlElement? element) {
+    if (element == null) return null;
     return Group(
-      contents: element
-          .findElements('media:content')
-          .map((e) => Content.parse(e))
-          .toList(),
-      credits: element
-          .findElements('media:credit')
-          .map((e) => Credit.parse(e))
-          .toList(),
-      category: Category.parse(findElementOrNull(element, 'media:category')),
-      rating: Rating.parse(findElementOrNull(element, 'media:rating')),
+      title: Title.parse(
+        findFirstElement(element, 'media:title'),
+      ),
+      contents: element.findElements('media:content').map((e) {
+        return Content.parse(e);
+      }).toList(),
+      thumbnails: element.findElements('media:thumbnail').map((e) {
+        return Thumbnail.parse(e);
+      }).toList(),
+      description: Description.parse(
+        findFirstElement(element, 'media:description'),
+      ),
+      community: Community.parse(
+        findFirstElement(element, 'media:community'),
+      ),
+      credits: element.findElements('media:credit').map((e) {
+        return Credit.parse(e);
+      }).toList(),
+      category: Category.parse(
+        findFirstElement(element, 'media:category'),
+      ),
+      rating: Rating.parse(
+        findFirstElement(element, 'media:rating'),
+      ),
     );
   }
 }
