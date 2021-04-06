816e4cdd99e575a1d2f39a4528efe4c0e41fddb7
N <nirvan@gmail.com>
2021-04-05 17:24:16 -0700

    Added support for YouTube feed

--- lib/domain/media/group.dart
+++ lib/domain/media/group.dart
@@ -2,17 +2,29 @@ import 'package:dart_rss/domain/media/category.dart';
 import 'package:dart_rss/domain/media/content.dart';
 import 'package:dart_rss/domain/media/credit.dart';
 import 'package:dart_rss/domain/media/rating.dart';
+import 'package:dart_rss/domain/media/thumbnail.dart';
+import 'package:dart_rss/domain/media/title.dart';
+import 'package:dart_rss/domain/media/description.dart';
+import 'package:dart_rss/domain/media/community.dart';
 import 'package:dart_rss/util/helpers.dart';
 import 'package:xml/xml.dart';
 
 class Group {
+  final Title? title;
   final List<Content> contents;
+  final List<Thumbnail> thumbnails;
+  final Description? description;
+  final Community? community;
   final List<Credit> credits;
   final Category? category;
   final Rating? rating;
 
   const Group({
+    this.title,
     this.contents = const <Content>[],
+    this.thumbnails = const <Thumbnail>[],
+    this.description,
+    this.community,
     this.credits = const <Credit>[],
     this.category,
     this.rating,
@@ -23,10 +35,22 @@ class Group {
       return null;
     }
     return Group(
+      title: Title.parse(
+        findElementOrNull(element, 'media:title'),
+      ),
       contents: element
           .findElements('media:content')
           .map((e) => Content.parse(e))
           .toList(),
+      thumbnails: element.findElements('media:thumbnail').map((e) {
+        return Thumbnail.parse(e);
+      }).toList(),
+      description: Description.parse(
+        findElementOrNull(element, 'media:description'),
+      ),
+      community: Community.parse(
+        findElementOrNull(element, 'media:community'),
+      ),
       credits: element
           .findElements('media:credit')
           .map((e) => Credit.parse(e))
