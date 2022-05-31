73d08529ceb7802560118ebc32e91bc3e0c44883
niravanu <nirvan@gmail.com>
2022-05-31 15:08:56 -0700

    version update

--- lib/domain/media/group.dart
+++ lib/domain/media/group.dart
@@ -6,57 +6,57 @@ import 'package:dart_rss/domain/media/thumbnail.dart';
 import 'package:dart_rss/domain/media/title.dart';
 import 'package:dart_rss/domain/media/description.dart';
 import 'package:dart_rss/domain/media/community.dart';
-import 'package:dart_rss/util/xml.dart';
+import 'package:dart_rss/util/helpers.dart';
 import 'package:xml/xml.dart';
 
 class Group {
   final Title? title;
-  final List<Content>? contents;
-  final List<Thumbnail>? thumbnails;
+  final List<Content> contents;
+  final List<Thumbnail> thumbnails;
   final Description? description;
   final Community? community;
-  final List<Credit>? credits;
+  final List<Credit> credits;
   final Category? category;
   final Rating? rating;
 
-  Group({
+  const Group({
     this.title,
-    this.contents,
-    this.thumbnails,
+    this.contents = const <Content>[],
+    this.thumbnails = const <Thumbnail>[],
     this.description,
     this.community,
-    this.credits,
+    this.credits = const <Credit>[],
     this.category,
     this.rating,
   });
 
-  static parse(XmlElement? element) {
-    if (element == null) return null;
+  static Group? parse(XmlElement? element) {
+    if (element == null) {
+      return null;
+    }
     return Group(
       title: Title.parse(
-        findFirstElement(element, 'media:title'),
+        findElementOrNull(element, 'media:title'),
       ),
-      contents: element.findElements('media:content').map((e) {
-        return Content.parse(e);
-      }).toList(),
+      contents: element
+          .findElements('media:content')
+          .map((e) => Content.parse(e))
+          .toList(),
       thumbnails: element.findElements('media:thumbnail').map((e) {
         return Thumbnail.parse(e);
       }).toList(),
       description: Description.parse(
-        findFirstElement(element, 'media:description'),
+        findElementOrNull(element, 'media:description'),
       ),
       community: Community.parse(
-        findFirstElement(element, 'media:community'),
+        findElementOrNull(element, 'media:community'),
       ),
-      credits: element.findElements('media:credit').map((e) {
-        return Credit.parse(e);
-      }).toList(),
-      category: Category.parse(
-        findFirstElement(element, 'media:category'),
-      ),
-      rating: Rating.parse(
-        findFirstElement(element, 'media:rating'),
-      ),
+      credits: element
+          .findElements('media:credit')
+          .map((e) => Credit.parse(e))
+          .toList(),
+      category: Category.parse(findElementOrNull(element, 'media:category')),
+      rating: Rating.parse(findElementOrNull(element, 'media:rating')),
     );
   }
 }
